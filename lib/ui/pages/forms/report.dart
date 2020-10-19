b68a1e2d424d52375a2489d1e2fbf447ab3ba9b4
David Martinez <davestringm@gmail.com>
2020-10-19 15:36:53 -0500

    chore (release): v1.1.0

--- lib/ui/pages/forms/report.dart
+++ lib/ui/pages/forms/report.dart
@@ -179,7 +179,7 @@ class _ReportState extends State<Report> {
                 margin: const EdgeInsets.only(top: 12.0, bottom: 25.0),
                 width: deviceWidth,
                 child: const Text(
-                  'Version v1.0.0',
+                  'Version v1.1.0',
                   textAlign: TextAlign.center,
                   style: TextStyle(
                     color: Colors.blueGrey,
